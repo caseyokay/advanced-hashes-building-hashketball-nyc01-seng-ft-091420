8dee3ba59bd2446ef6170c2901464b9dd5580e90
Casey B <caseyoksana@gmail.com>
2020-09-10 03:27:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -59,7 +59,8 @@ def game_hash{
   away:{ 
     team_name:"Charlotte Hornets",
     colors: ["Turquoise", "Purple"],
-    players: [  {player_name:"Jeff Adrien", 
+    players: [
+      {player_name:"Jeff Adrien", 
       number:4, 
       shoe:18, 
       points:10, 
