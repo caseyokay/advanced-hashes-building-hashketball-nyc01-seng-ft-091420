05e840ade1bbde5e627eb0c1887bb0edf0556948
Casey B <caseyoksana@gmail.com>
2020-09-10 03:11:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,7 +3,7 @@ def game_hash {
   home:{
     team_name:"",
     colors: [],
-    players: [{player_name:, number:,}]
+    players: [{player_name:, number:, shoe:, points:, rebounds:, assists:, steals:, blocks:, slam_dunks:,}]
   },
   away:{}
 }
